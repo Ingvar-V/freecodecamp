2d8030fc2d45d7d4fbfef4fbed39c0223834c1bc
Ingvar-V <ingvarvolkov@gmail.com>
2022-12-05 19:37:07 +0300

    Update bike-shop.sh

--- bikes/bike-shop.sh
+++ bikes/bike-shop.sh
@@ -38,13 +38,43 @@ RENT_MENU(){
     # ask for bike to rent
     echo -e '\nWhich one would you like to rent?'
     read BIKE_ID_TO_RENT
-     
+
 
     # if input is not a number
-    # send to main menu
+    if [[ ! $BIKE_ID_TO_RENT =~ ^[0-9]+$ ]]
+    then
+       # send to main menu
+       MAIN_MENU "That is not a valid bike number."
+    else
+      # get bike availability
+      BIKE_AVAILABILITY=$($PSQL "SELECT available FROM bikes WHERE bike_id = $BIKE_ID_TO_RENT AND available = true") 
+
+      # if not available
+      if [[ -z $BIKE_AVAILABILITY ]] 
+      then
+         # send to main menu
+         MAIN_MENU "That bike is not available."
+      else
+         # get customer info
+         echo -e "\nWhat's your phone number?"
+         read PHONE_NUMBER 
+         CUSTOMER_NAME=$($PSQL "SELECT name FROM customers WHERE phone='$PHONE_NUMBER'")
+         
+         # if customer doesn't exist
+         if [[ -z $CUSTOMER_NAME ]]
+         then
+            # get new customer name
+            echo -e "\nWhat's your name?"
+            read CUSTOMER_NAME
+
+            # insert new customer
+            INSERT_CUSTOMER_RESULT=$($PSQL "INSERT INTO customers(name, phone) VALUES('$CUSTOMER_NAME', '$PHONE_NUMBER')") 
+         fi      
+      fi   
+    fi
+    
   fi  
 
-  # send to main menu
 }
 
 RETURN_MENU(){
